68bb671929d5e6f9afc5541b391a0b07500d4696
tmulle204 <travis_mullen@comcast.com>
2018-09-07 15:55:34 -0400

    format js

--- rollup.build.js
+++ rollup.build.js
@@ -3,10 +3,10 @@ import commonjs from 'rollup-plugin-commonjs'
 import pkg from './package.json'
 
 export default {
-    input: 'src/truncation-title.element.js',
-    output: { file: pkg.module, format: 'es' },
-    plugins: [
-      resolve(), // so Rollup can find `resize-observer-polyfill`
-      commonjs() // so Rollup can convert `resize-observer-polyfill` to an ES module
-    ]
-  }
+  input: 'src/truncation-title.element.js',
+  output: { file: pkg.module, format: 'es' },
+  plugins: [
+    resolve(), // so Rollup can find `resize-observer-polyfill`
+    commonjs() // so Rollup can convert `resize-observer-polyfill` to an ES module
+  ]
+}
--- rollup.serve.js
+++ rollup.serve.js
@@ -3,16 +3,16 @@ import serve from 'rollup-plugin-serve'
 import livereload from 'rollup-plugin-livereload'
 
 export default {
-    input: build.input,
-    output: build.output,
-    plugins: [
-      ...build.plugins,
-      serve({
-        open: true,
-        contentBase: ['','dist']
-      }),
-      livereload({
-        watch: ['src', 'index.html']
-      })
-    ]
-  }
+  input: build.input,
+  output: build.output,
+  plugins: [
+    ...build.plugins,
+    serve({
+      open: true,
+      contentBase: ['', 'dist']
+    }),
+    livereload({
+      watch: ['src', 'index.html']
+    })
+  ]
+}
