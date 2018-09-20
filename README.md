51a53879aedddce8065cc2c0893c5add91f53c43
tmulle204 <travis_mullen@comcast.com>
2018-09-20 11:12:07 -0400

    change truncation to truncate

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Truncation Title :scissors:
+# Truncate Title :scissors:
 
 <!-- [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com) -->
 
--- rollup.build.js
+++ rollup.build.js
@@ -3,7 +3,7 @@ import commonjs from 'rollup-plugin-commonjs'
 import pkg from './package.json'
 
 export default {
-  input: 'src/truncation-title.element.js',
+  input: 'src/truncate-title.element.js',
   output: { file: pkg.module, format: 'es' },
   plugins: [
     resolve(), // so Rollup can find `resize-observer-polyfill`
